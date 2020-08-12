e9d1f35359202cc6a8b82e3cd3d28a969ee03fd7
jaswanth-jash <jaswanthg12@gmail.com>
2020-08-12 20:47:28 +0530

    pgm

--- src/programs/Car.java
+++ src/programs/Car.java
@@ -1,7 +1,26 @@
 package programs;
-
-public class Car {
-    public static void main(String arg[]){
-        System.out.println("benz");
-    }
-}
+public class Car{
+         String brand = "BMW";
+         String model = "dezire";
+         String color ="red";
+         int cost = 2000000;
+         void accelerate() {
+         System.out.println("accelerating");
+         }
+         void slowDown(){
+         System.out.println("slow dowing");
+         }
+         void gearChange(){
+         System.out.println("gear changing");
+         }
+public static void main(String arg[]){
+        Car c1 = new Car();
+        System.out.println(c1.brand);
+        System.out.println(c1.model);
+        System.out.println(c1.color);
+        System.out.println(c1.cost);
+        c1.accelerate();
+        c1.slowDown();
+        c1.gearChange();
+        }
+        }
