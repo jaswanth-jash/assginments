43b5d6ceaba49bf123619cf75a894fe02573e34b
jaswanth-jash <jaswanthg12@gmail.com>
2020-08-17 21:18:01 +0530

    AAAA

---
+++ src/programs/Integer.java
@@ -0,0 +1,13 @@
+package programs;
+
+public class Integer {
+    public static void main(String args[]){
+        int num ;
+        num = 21;
+        System.out.println(num);
+       // System.out.println("The value of num is "  + num);
+        int num1 = num * 2;
+        System.out.print("The value of num1 is "  );
+        System.out.println(num1);
+    }
+}
---
+++ src/programs/Number.java
@@ -0,0 +1,14 @@
+package programs;
+
+import java.util.Scanner;
+
+public class Number {
+    public static void main(String args[]) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("enter the value");
+        int num = scan.nextInt();
+        System.out.println(num);
+        int num1 =num *2;
+        System.out.println(num1);
+    }
+}
