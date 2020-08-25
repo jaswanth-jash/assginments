eee961c9e01f2699bb5be061be73218b9bfa7b76
jaswanth-jash <jaswanthg12@gmail.com>
2020-08-25 07:43:42 +0530

    AAAA

---
+++ src/statementsloops/MyFamily.java
@@ -0,0 +1,29 @@
+package statementsloops;
+
+public class MyFamily {
+    public static void main(String args[]) {
+        String name = "subhashini ";
+        int age;
+        if (name.equals("govardhan")) {
+            for (age = 45; age <= 50; age++) {
+                if (age == 50) {
+                    System.out.println("His name is Govardhan.He is my father ,his age is: " + age);
+                }
+            }
+        } else if (name.equals("subhashini")) {
+            for (age = 42; age <= 46; age++) {
+                if (age == 45) {
+                    System.out.println("Her name is Subhashini. she is my mother,her age is: " + age);
+                }
+            }
+        } else if (name.equals("jaswanth")) {
+            for (age = 21; age <= 23; age++) {
+                if (age == 23) {
+                    System.out.println("His name is Jaswanth. That's me,my age is: " + age);
+                }
+            }
+        } else {
+            System.out.println("His name is SaiKumar .He is my brother,his age is:20 ");
+        }
+    }
+}
---
+++ src/statementsloops/PositiveOrNegative.java
@@ -0,0 +1,14 @@
+package statementsloops;
+
+public class PositiveOrNegative {
+    public static void main(String args[]) {
+        int value = 21;
+        if (value < 0) {
+            System.out.println("The value is Negative: " + value);
+        } else if (value > 0) {
+            System.out.println("The value is Positive: " + value);
+        } else {
+            System.out.println("The value is Zero " + value);
+        }
+    }
+}
