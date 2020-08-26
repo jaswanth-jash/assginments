66da33c4f0d8dd6d8004ea052d19245dce9e060f
jaswanth-jash <jaswanthg12@gmail.com>
2020-08-26 21:06:24 +0530

    codes checkin

---
+++ src/statementsloops/RatingShows.java
@@ -0,0 +1,37 @@
+package statementsloops;
+
+public class RatingShows {
+    public static void main(String args[]) {
+        System.out.println("Rating of news channel in india\n");
+        String showName = "tv9";
+        int rating;
+        if (showName.equals("tv9")) {
+            for (rating = 1; rating <= 5; rating++) {
+                if (rating == 5) {
+                    System.out.println(" tv9 channel rating :" + rating);
+                }
+            }
+        } else if (showName.equals("etv")) {
+            for (rating = 1; rating <= 5; rating++) {
+                if (rating == 4) {
+                    System.out.println("etv channel rating :" + rating);
+                }
+            }
+        } else if (showName.equals("ABN")) {
+            for (rating = 1; rating <= 5; rating++) {
+                if (rating == 3) {
+                    System.out.println("ABN channel rating :" + rating);
+                }
+            }
+        } else if (showName.equals("sakshi")) {
+            for (rating = 1; rating <= 5; rating++) {
+                if (rating == 2) ;
+                {
+                    System.out.println("Sakshi channel rating : " + rating);
+                }
+            }
+        } else {
+            System.out.println("Other news channel rating below 2");
+        }
+    }
+}
