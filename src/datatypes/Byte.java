f53fb5fa3d3af5ae9d4b09ed66db0795ead25dd3
jaswanth-jash <jaswanthg12@gmail.com>
2020-08-30 19:44:10 +0530

    code datatypes

---
+++ src/datatypes/Byte.java
@@ -0,0 +1,11 @@
+package datatypes;
+
+public class Byte {
+    public static void main(String args[]) {
+        byte age = 23;
+        System.out.println("My age " + age);
+        // System.out.println("My mom age " + age);
+        //System.out.println("My dad age " + age);
+        //System.out.println("My brother age " + age);
+    }
+}
