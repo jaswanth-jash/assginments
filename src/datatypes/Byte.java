9697df80ba8c31565ba7b6d5c852da55e2a4d455
jaswanth-jash <jaswanthg12@gmail.com>
2020-08-30 20:00:19 +0530

    AAA

---
+++ src/datatypes/Byte.java
@@ -0,0 +1,11 @@
+package datatypes;
+
+public class Byte {
+    public static void main(String args[]) {
+        byte age = 23;
+        System.out.println("My age " + age);
+        //System.out.println("My Mom age " + age);
+        //System.out.println("My Dad age " + age);
+        //System.out.println("My Brother age " + age);
+    }
+}
---
+++ src/datatypes/Byte2.java
@@ -0,0 +1,10 @@
+package datatypes;
+
+public class Byte2 {
+    public static void main(String args[]) {
+        byte a = 127;
+        double b = a;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
