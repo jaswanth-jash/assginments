060e1428a45ca34e4805110cbd526b8e5d96d80d
jaswanth-jash <jaswanthg12@gmail.com>
2020-08-31 21:02:12 +0530

    AAA

---
+++ src/datatypes/Boolean.java
@@ -0,0 +1,10 @@
+package datatypes;
+
+public class Boolean {
+    public static void main(String args[]) {
+        boolean a = true;
+        boolean b = false;
+        System.out.println("Your telling " + a);
+        System.out.println("Your telling " + b);
+    }
+}
---
+++ src/datatypes/Boolean2.java
@@ -0,0 +1,18 @@
+package datatypes;
+
+public class Boolean2 {
+    public static void main(String args[]) {
+        int a, b, c;
+        a = 15;
+        b = 10;
+        c = 8;
+        System.out.println(a > b);
+        System.out.println(b > c);
+        System.out.println(a > c);
+        System.out.println(c > b);
+        System.out.println(b > a);
+        System.out.println(c > a);
+        System.out.println(a == 15);
+        System.out.println(a == 10);
+    }
+}
