9c3990df9ef7051d52fb8c6eb4767b962bce278e
jaswanth-jash <jaswanthg12@gmail.com>
2020-09-01 15:28:17 +0530

    AAA

---
+++ src/datatypes/Char3.java
@@ -0,0 +1,9 @@
+package datatypes;
+
+public class Char3 {
+    public static void main(String args[]) {
+        for (int i = 0; i <= 255; i++) {
+            System.out.println("The ASCII value of " + (char) i + " = " + i);
+        }
+    }
+}
---
+++ src/datatypes/Char4.java
@@ -0,0 +1,10 @@
+package datatypes;
+
+public class Char4 {
+    public static void main(String args[]) {
+        //Here we print capital letter of alphabets (A to Z)
+        for (int i = 65; i <= 90; i++) {
+            System.out.println("The ASCII value of " + (char) i + " = " + i);
+        }
+    }
+}
