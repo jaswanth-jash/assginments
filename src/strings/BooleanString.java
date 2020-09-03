32067727e293764f455e9c831382bbe77812b458
jaswanth-jash <jaswanthg12@gmail.com>
2020-09-03 21:04:51 +0530

    AAAAA

--- src/strings/BooleanString.java
+++ src/strings/BooleanString.java
@@ -2,19 +2,24 @@ package strings;
 
 public class BooleanString {
     public static void main(String args[]) {
-        String[] student = {"Jaswanth", "Rekha", "Lekha", "Sai", "Pravalika"};  //similar group lo elements
-        String[] staff = {"Ayyappa", "Radha"};
+        //Arrays are used to store multiple values in a single variable, instead of declaring separate variables for each value
+        //similar group lo elements
+        String[][] array = {{"Jaswanth", "Rekha", "Lekha", "Sai", "Pravalika"}, {"Ayyappa", "Radha"}};
         boolean flag = true;
         if (flag = true) {
-            for (int i = 0; i <= student.length; i++) {
-                System.out.println("Total number of fresher :" + student.length);
+            for (int i = 0; i <= array[0].length; i++) {
+                System.out.println("Total number of fresher :" + array[0].length);
                 break;
             }
-        } else {
-            for (int i = 0; i <= staff.length; i++) {
-                System.out.println("Total number of staff :" + staff.length);
+        }
+        if (flag = true) {
+            for (int i = 0; i <= array[1].length; i++) {
+                System.out.println("Total number of staff :" + array[1].length);
                 break;
             }
+        }
+        else {
+            System.out.println("Please select boolean value");
         }
     }
 }
---
+++ src/strings/Logical1.java
@@ -0,0 +1,15 @@
+package strings;
+
+public class Logical1 {
+    public static void main(String[] args) {
+        float number1 = 120.345f;
+        float number2 = 345.21f;
+        float number3 = 234.21f;
+        if (number1 < number2 && number1 < number3) {
+            System.out.println("Smallest Number is number1");
+        }
+        if (number3 > number2 || number3 > number1) {
+            System.out.println("number3 is not smallest");
+        }
+    }
+}
