f1df304e2c9ec82196c9f2878567f4bf3c54cc04
jaswanth-jash <jaswanthg12@gmail.com>
2020-09-04 22:18:25 +0530

    AAAAA

---
+++ src/strings/BooleanStudent.java
@@ -0,0 +1,44 @@
+package strings;
+
+public class BooleanStudent {
+    public static void main(String[] args) {
+        String name = "jaswanth";
+        boolean flag = false;
+        int i;
+        if (flag = true) {
+            if (name.equals("jaswanth")) {
+                System.out.println("Sometimes are doing good programs, sometimes u are not doing ");
+            }
+            if (name.equals("rekha")) {
+                System.out.println("Sometimes are doing good ,you do more programs per day ");
+            }
+            if (name.equals("lekha")) {
+                System.out.println("You are doing good programs,but u want push code in git  ");
+            }
+            if (name.equals("pravalika")) {
+                System.out.println("Now you are in on track keep going ");
+            }
+            if (name.equals("sai")) {
+                System.out.println("You are also on track ");
+            }
+        } else if (flag = false) {
+            if (name.equals("jaswanth")) {
+                System.out.println("Next time can u explain in english ");
+            }
+            if (name.equals("rekha")) {
+                System.out.println("you do more programs and code push to git ");
+            }
+            if (name.equals("lekha")) {
+                System.out.println("  You do more programs and push code in git  ");
+            }
+            if (name.equals("pravalika")) {
+                System.out.println("You can spent more time  ");
+            }
+            if (name.equals("sai")) {
+                System.out.println("You do more programs and understand the concepts ");
+            }
+        } else {
+            System.out.println("Can u choose boolean value");
+        }
+    }
+}
