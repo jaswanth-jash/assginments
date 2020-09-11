62e046456a069dd0f5cbe308c5566d13de533e41
jaswanth-jash <jaswanthg12@gmail.com>
2020-09-11 21:04:18 +0530

    AAAAAA

---
+++ src/strings/Return1.java
@@ -0,0 +1,12 @@
+package strings;
+
+public class Return1 {
+    int display() {
+        return 25;
+    }
+
+    public static void main(String args[]) {
+        Return1 e = new Return1();
+        System.out.println(e.display());
+    }
+}
