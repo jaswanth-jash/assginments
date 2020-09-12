b5607da6ee335ece0e803fa1db662a7c20568083
jaswanth-jash <jaswanthg12@gmail.com>
2020-09-12 19:23:03 +0530

    AAAAAA

---
+++ src/strings/Return2.java
@@ -0,0 +1,12 @@
+package strings;
+
+public class Return2 {
+    void display() {
+        return 25;
+    }
+
+    public static void main(String args[]) {
+        Return2 e = new Return2();
+        System.out.println(e.display());
+    }
+}
