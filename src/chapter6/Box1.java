ed0cbe1db537eee8cf13eead8d6f65b491c794f8
jaswanth-jash <jaswanthg12@gmail.com>
2020-09-13 21:18:15 +0530

    AAAAA

--- src/chapter6/Box1.java
+++ src/chapter6/Box1.java
@@ -1,4 +1,18 @@
 package chapter6;
 
 public class Box1 {
+    int width;
+    int height;
+    int depth;
+
+    public static void main(String args[]) {
+        Box1 b = new Box1();
+        double volume;
+        //assign to values b instances variables
+        b.width = 25;
+        b.height = 15;
+        b.depth = 10;
+        volume = b.width * b.height * b.depth;
+        System.out.println("total volume = " + volume);
+    }
 }
