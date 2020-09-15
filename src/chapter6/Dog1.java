3b75f9331c644b05475f1092b3facec627e5a7e0
jaswanth-jash <jaswanthg12@gmail.com>
2020-09-15 19:39:50 +0530

    AAAA

---
+++ src/chapter6/Dog1.java
@@ -0,0 +1,18 @@
+package chapter6;
+
+public class Dog1 {
+    String name;
+    int cost;
+    String bread;
+
+    public static void main(String args[]) {
+        Dog1 d = new Dog1();
+        d.name = "jockey";
+        d.cost = 10000;
+        d.bread = "german";
+        System.out.println(d.name);
+        System.out.println(d.cost);
+        System.out.println(d.bread);
+
+    }
+}
