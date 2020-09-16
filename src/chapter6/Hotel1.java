71b6f5f37d5aa33a178d7f346901c0b22dc528ec
jaswanth-jash <jaswanthg12@gmail.com>
2020-09-16 20:31:06 +0530

    MMMMM

---
+++ src/chapter6/Hotel1.java
@@ -0,0 +1,15 @@
+package chapter6;
+
+public class Hotel1 {
+    String name;
+    int star;
+    String quality;
+
+    public static void main(String args[]) {
+        Hotel1 h = new Hotel1();
+       // d.name = "Bliss"
+        System.out.println(h.name = "Bliss");
+        System.out.println(h.star = 5);
+        System.out.println(h.quality = "Good");
+    }
+}
