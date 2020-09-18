9cda42407c8e829c2ffdf58b1761c8b6bd6ec4f3
jaswanth-jash <jaswanthg12@gmail.com>
2020-09-18 19:29:23 +0530

    MMMMM

---
+++ src/chapter6/Box3.java
@@ -0,0 +1,25 @@
+package chapter6;
+
+public class Box3 {
+    int width;
+    int height;
+    int depth;
+
+    void volume() {
+        System.out.print(" volume is :");
+        System.out.println(width * height * depth);
+    }
+
+    public static void main(String args[]) {
+        Box3 b = new Box3();
+        //assign to values b instances variables
+        b.width = 25;
+        b.height = 15;
+        b.depth = 10;
+
+        //display box volume
+        b.volume();
+
+
+    }
+}
