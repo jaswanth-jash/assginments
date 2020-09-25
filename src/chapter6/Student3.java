3357f5cf6bcef4eafa3ea07f302c3a3424b4b0c8
jaswanth-jash <jaswanthg12@gmail.com>
2020-09-25 21:11:30 +0530

    this

---
+++ src/chapter6/Student3.java
@@ -0,0 +1,24 @@
+package chapter6;
+
+public class Student3 {
+    int id;
+    String name;
+    String gender;
+
+    //Parametrised constructor
+    Student3(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    void res() {
+        System.out.println("id:" + id + " name :" + name + "  ");
+    }
+
+    public static void main(String[] args) {
+        Student3 s = new Student3(1102, "jay");
+        Student3 s1 = new Student3(1102, "yojith");
+        s.res();
+        s1.res();
+    }
+}
