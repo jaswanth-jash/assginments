1b7bd32d54d34c8b442687d46ef59f90ee30700a
jaswanth-jash <jaswanthg12@gmail.com>
2020-09-26 20:09:08 +0530

    this

---
+++ src/chapter6/Dog4.java
@@ -0,0 +1,24 @@
+package chapter6;
+
+public class Dog4 {
+    String name;
+    int cost;
+    String bread;
+
+    Dog4(String name, int cost, String bread) {
+        this.name = name;
+        this.cost = cost;
+        this.bread = bread;
+    }
+
+    void result() {
+        System.out.println("name : " + name + ", cost : " + cost + ", bread : " + bread + " ");
+    }
+
+    public static void main(String[] args) {
+        Dog4 d = new Dog4("husky", 30000, "doberman");
+        Dog4 d1 = new Dog4("rockey", 10000, "bulldog");
+        d.result();
+        d1.result();
+    }
+}
