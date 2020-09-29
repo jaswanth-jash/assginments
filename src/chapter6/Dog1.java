847ec2afd2923bde8d80a652395f44a6a3f3cf4b
jaswanth-jash <jaswanthg12@gmail.com>
2020-09-29 21:43:29 +0530

    this

--- src/chapter6/Dog1.java
+++ src/chapter6/Dog1.java
@@ -9,7 +9,7 @@ public class Dog1 {
         Dog1 d = new Dog1();
         d.name = "jockey";
         d.cost = 10000;
-        d.bread = "german";
+        d.bread = "germansheped";
         System.out.println(d.name);
         System.out.println(d.cost);
         System.out.println(d.bread);
