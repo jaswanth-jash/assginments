6be4900913651c789d550f0edc5fc43e88ab3a28
jaswanth-jash <jaswanthg12@gmail.com>
2020-09-30 15:50:28 +0530

    for

---
+++ src/revision/For1.java
@@ -0,0 +1,10 @@
+package revision;
+
+public class For1 {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            System.out.println("i want to become a good fullStack developer");
+        }
+    }
+}
---
+++ src/revision/For2.java
@@ -0,0 +1,11 @@
+package revision;
+
+public class For2 {
+    public static void main(String[] args) {
+        System.out.println("here i print even nos");
+        int n = 25;
+        for (int i = 0; i <= n; i = i + 2) {
+            System.out.println(i);
+        }
+    }
+}
