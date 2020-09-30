622be66a29cb67b7c7e58785add028fd7fcc345a
jaswanth-jash <jaswanthg12@gmail.com>
2020-09-30 22:28:36 +0530

    for

---
+++ src/revision/While1.java
@@ -0,0 +1,10 @@
+package revision;
+
+public class While1 {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i < 25) {
+            System.out.println(i = i + 2);
+        }
+    }
+}
