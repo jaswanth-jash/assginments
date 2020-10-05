68b0f39fcd7b434e619c0699f8a376e670b12551
jaswanth-jash <jaswanthg12@gmail.com>
2020-10-05 19:59:48 +0530

    for

---
+++ src/revision/Pyramid.java
@@ -0,0 +1,22 @@
+package revision;
+
+public class Pyramid {
+    public static void main(String[] args) {
+        int i, j, n, s, x;
+        // System.out.print ("Input number of rows : ");
+        //Scanner in = new Scanner(System.in);
+        n = 5;
+
+        s = n + 4 - 1;
+        for (i = 1; i <= n; i++) {
+            for (x = s; x != 0; x--) {
+                System.out.print(" ");
+            }
+            for (j = 1; j <= i; j++) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+            s--;
+        }
+    }
+}
