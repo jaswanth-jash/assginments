662e103e84212c5d7c237a731af62c0bdb46d7f0
jaswanth-jash <jaswanthg12@gmail.com>
2020-10-05 19:51:08 +0530

    for

---
+++ src/revision/Cude1.java
@@ -0,0 +1,13 @@
+package revision;
+
+public class Cude1 {
+    public static void main(String[] args) {
+        int i, n;
+        //System.out.println("Input number of terms : ");
+        // Scanner in = new Scanner(System.in);
+        n = 10;
+        for (i = 1; i <= n; i++) {
+            System.out.println("Number is : " + i + " and cube of " + i + " is : " + (i * i * i));
+        }
+    }
+}
