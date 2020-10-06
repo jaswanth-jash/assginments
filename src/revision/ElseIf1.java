0924ff2c69451813551fef5922c068fd0f23d1b0
jaswanth-jash <jaswanthg12@gmail.com>
2020-10-06 21:48:50 +0530

    if

---
+++ src/revision/ElseIf1.java
@@ -0,0 +1,17 @@
+package revision;
+
+public class ElseIf1 {
+    public static void main(String[] args) {
+        // Scanner in = new Scanner(System.in);
+        // System.out.print("Input a number: ");
+        int n = 5;
+
+        if (n > 0) {
+            System.out.println("Number is positive");
+        } else if (n < 0) {
+            System.out.println("Number is negative");
+        } else {
+            System.out.println("Number is zero");
+        }
+    }
+}
