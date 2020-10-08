fc32caf469f51019f7d8109cdd5edec956211ab8
jaswanth-jash <jaswanthg12@gmail.com>
2020-10-08 21:32:26 +0530

    for

---
+++ src/training/Bike.java
@@ -0,0 +1,13 @@
+package training;
+
+public class Bike {
+    Bike() {
+        System.out.println("Bike class is created");
+    }
+
+    //main method
+    public static void main(String args[]) {
+//calling a default constructor
+        Bike b = new Bike();
+    }
+}
