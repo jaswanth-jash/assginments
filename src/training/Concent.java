0e66ac04d820cae2b4e3534924b209c86f57fde1
jaswanth-jash <jaswanthg12@gmail.com>
2020-10-09 20:01:37 +0530

    for

---
+++ src/training/Concent.java
@@ -0,0 +1,29 @@
+package training;
+
+public class Concent {
+    public static void main(String[] args) {
+        String name = "Ayyappa";
+        if (name.equals("Ayyappa")) {
+            System.out.println("He is a fullStack developer " +
+                    "He is from USA " + " He has around 12+ year Experiance in java ");
+        } else if (name.equals("Radha")) {
+            System.out.println("He is our project manager " + "He is from canada " + " He has more than 12 year experiance in java ");
+        } else if (name.equals("venkat")) {
+            System.out.println("He is a CEO of our company " + " He is from Bangalore " + " He has more than 15 year experiance in fullStack developer ");
+        } else if (name.equals("Vamsee")) {
+            System.out.println("He is also one of the developer " + " He has 10 year experiance in java");
+        } else if (name.equals("Gopi")) {
+            System.out.println("He is backend developer " + "he has 2 year experiance in java");
+        } else if (name.equals("venkat y")) {
+            System.out.println("he is also a fullStack develope r" + " he has 8 year of experiance");
+        } else if (name.equals("Narshimulu")) {
+            System.out.println("He is a QA team lead " + " with him uma and vidya also QA team members");
+        } else if (name.equals("Deepthi")) {
+            System.out.println("She is operating team member and also QA teamate");
+        } else if (name.equals("Trainers")) {
+            System.out.println("jaswanth,rekha,nikitha,pooja,mounisha,pavithra");
+        } else {
+            System.out.println("This name is not found");
+        }
+    }
+}
