8f97201ba697ae58d4b14035645ed92a2698381b
jaswanth-jash <jaswanthg12@gmail.com>
2020-10-10 21:23:44 +0530

    constructor

--- src/training/MyArray2.java
+++ src/training/MyArray2.java
@@ -4,12 +4,12 @@ public class MyArray2 {
     public static void main(String[] args)
     {
 
-        int arr [][][] = { {4, 1, 2}, {  7, 5, 3 }, { 8, 9, 6} };
+        int arr [][] = { {4, 1, 2}, {  7, 5, 3 }, { 8, 9, 6} };
 
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                for (int k = 0; k < 3; k++)
-                System.out.print(arr[i][j][k] + " ");
+                //for (int k = 0; k < 3; k++)
+                System.out.print(arr[i][j] + " ");
             }
 
             System.out.println();
