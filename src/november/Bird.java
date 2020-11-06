4a880829e488da19f8b74c19662aae6c0076844f
jaswanth-jash <jaswanthg12@gmail.com>
2020-11-06 22:01:22 +0530

    class

---
+++ src/november/Bird.java
@@ -0,0 +1,54 @@
+package november;
+
+abstract public class Bird {
+    abstract void fly();
+
+    abstract void eat();
+}
+
+abstract class Eagle extends Bird {
+    void eat() {
+        System.out.println("Eagle is heating");
+    }
+
+    static class Sparrow extends Bird {
+
+        @Override
+        void fly() {
+            System.out.println("Sparrow is flying");
+        }
+
+        @Override
+        void eat() {
+            System.out.println("Sparrow is eating");
+        }
+    }
+
+    static class GoldenEagle extends Eagle {
+
+        @Override
+        void fly() {
+            System.out.println("Flying on mountains");
+        }
+    }
+
+    static class SerpentEagle extends Eagle {
+
+        @Override
+        void fly() {
+            System.out.println("Flying on oceans");
+        }
+    }
+
+    public static void main(String args[]) {
+        Bird s = new Sparrow();
+        s.fly();
+        s.eat();
+        Eagle e = new GoldenEagle();
+        e.eat();
+        e.fly();
+        Eagle e1 = new SerpentEagle();
+        e1.eat();
+        e1.fly();
+    }
+}
