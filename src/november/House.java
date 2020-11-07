60df8f072632bb15cba13abaccd1507d4114d359
jaswanth-jash <jaswanthg12@gmail.com>
2020-11-07 21:56:39 +0530

    class

---
+++ src/november/House.java
@@ -0,0 +1,27 @@
+package november;
+
+public interface House {
+    void flates();
+}
+
+class House1 implements House {
+    public void flates() {
+        System.out.println("3 BHK house");
+    }
+
+    static class House2 implements House {
+
+        @Override
+        public void flates() {
+            System.out.println("2 BHK house ");
+        }
+    }
+
+    public static void main(String args[]) {
+        House h = new House1();
+        House h1 = new House2();
+        h.flates();
+        h1.flates();
+    }
+}
+
