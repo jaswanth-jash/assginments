6ebf1d374420fd2f3d7445d5f8c6fd14dcd5ad9e
jaswanth-jash <jaswanthg12@gmail.com>
2020-11-08 22:56:39 +0530

    oops

---
+++ src/november/Trainer.java
@@ -0,0 +1,30 @@
+package november;
+
+public interface Trainer {
+    public void first();
+}
+
+interface Trainer2 {
+    public void second();
+}
+
+class Team implements Trainer, Trainer2 {
+
+    @Override
+    public void first() {
+        System.out.println("Ayyappa is trainer");
+    }
+
+    @Override
+    public void second() {
+        System.out.println("Vamsi is aslo a trainer");
+
+    }
+
+    public static void main(String args[]) {
+        Team t = new Team();
+        t.first();
+        t.second();
+    }
+}
+
