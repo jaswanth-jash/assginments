472dfce3f93dfec399ce549666477fc92f6e7ade
jaswanth-jash <jaswanthg12@gmail.com>
2020-11-16 17:14:45 +0530

    class

---
+++ src/november/Count1.java
@@ -0,0 +1,17 @@
+package november;
+
+public class Count1 {
+    static int count = 0;
+
+    Count1() {
+        count++;
+        System.out.println(count);
+    }
+
+    public static void main(String args[]) {
+        Count1 c = new Count1();
+        Count1 c1 = new Count1();
+        Count1 c2 = new Count1();
+    }
+}
+
