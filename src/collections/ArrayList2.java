51e7a6a6c6496f283c45d2fa14899da2aa4f299e
jaswanth-jash <jaswanthg12@gmail.com>
2020-11-27 22:25:10 +0530

    collections

---
+++ src/collections/ArrayList2.java
@@ -0,0 +1,22 @@
+package collections;
+
+import java.util.*;
+
+public class ArrayList2 {
+    public static void main(String args[]) {
+        ArrayList<String> a = new ArrayList<String>();//Creating arraylist
+        a.add("ayyappa");
+        a.add("radha");
+        a.add("vamsi");
+        a.add("jashu");
+        a.add(0, "jagan");
+        a.remove(3);
+        a.get(3);
+        a.indexOf("jashu");
+        a.size();
+        Iterator itr = a.iterator();
+        while (itr.hasNext()) {
+            System.out.println(itr.next());
+        }
+    }
+}
