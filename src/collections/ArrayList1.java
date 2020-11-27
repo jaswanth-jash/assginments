825660b40f4ac275d2e7787f9c7592cfc57e07de
jaswanth-jash <jaswanthg12@gmail.com>
2020-11-27 22:15:43 +0530

    collections

---
+++ src/collections/ArrayList1.java
@@ -0,0 +1,23 @@
+package collections;
+
+import java.util.*;
+
+public class ArrayList1 {
+    public static void main(String args[]) {
+        ArrayList<String> list = new ArrayList<String>();//Creating arraylist
+        list.add("jashu");//Adding object in arraylist
+        list.add("sai");
+        list.add("jay");
+        list.add("yojith");
+        list.add(null);
+        list.add(2, "ayyappa");
+        //list.add(10);
+        System.out.println(list);
+        list.get(1);
+        System.out.println(list);
+        list.remove(1);
+        list.add("sai");
+        System.out.println(list);
+    }
+}
+
