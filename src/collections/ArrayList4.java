fa88c78c2bc4469480d7cd9ec039a0612ce41a3b
jaswanth-jash <jaswanthg12@gmail.com>
2020-12-08 21:06:44 +0530

    collection

--- src/collections/ArrayList4.java
+++ src/collections/ArrayList4.java
@@ -11,6 +11,7 @@ public class ArrayList4 {
         al.add("sai");
         System.out.println(al);
         al.set(2, "jay");
+        al.remove(3);
         System.out.println(al);
         System.out.println(al.size());
     }
