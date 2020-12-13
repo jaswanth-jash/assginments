bb8dbacb9f1586d37e935a99d790d043a18ed08c
jaswanth-jash <jaswanthg12@gmail.com>
2020-12-13 21:23:32 +0530

    collection

---
+++ src/collections/ArrayList5.java
@@ -0,0 +1,20 @@
+package collections;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class ArrayList5 {
+    public static void main(String args[]) {
+        ArrayList<String> jj = new ArrayList<String>();
+        jj.add(0, "jaswanth");
+        jj.add("pooja");
+        jj.add("nikitha");
+        jj.add(3, "pavithra");
+        jj.add("raskith");
+        jj.set(4, "raskith");
+        Iterator itr = jj.iterator();
+        while (itr.hasNext()) {
+            System.out.println(itr.next());
+        }
+    }
+}
