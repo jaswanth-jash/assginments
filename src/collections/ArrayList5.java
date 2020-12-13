be9c1450256e3915e211949012224bc53e06bef2
jaswanth-jash <jaswanthg12@gmail.com>
2020-12-13 21:26:11 +0530

    collection

--- src/collections/ArrayList5.java
+++ src/collections/ArrayList5.java
@@ -12,6 +12,7 @@ public class ArrayList5 {
         jj.add(3, "pavithra");
         jj.add("raskith");
         jj.set(4, "raskith");
+        jj.remove(4);
         Iterator itr = jj.iterator();
         while (itr.hasNext()) {
             System.out.println(itr.next());
