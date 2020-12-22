b8c07ec3a887dc6b4479757020e23426011e21b5
jaswanth-jash <jaswanthg12@gmail.com>
2020-12-22 23:02:31 +0530

    changes

---
+++ src/collections/ArrayList6.java
@@ -0,0 +1,23 @@
+package collections;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class ArrayList6 {
+    public static void main(String args[]) {
+        ArrayList<String> a = new ArrayList<String>();//Creating arraylist
+        a.add("jashu");
+        a.add("mahesh");
+        a.add("sai");
+        a.add("rishi");
+        a.add(0, "gowtham");
+        a.remove(3);
+        a.add(4,"ajith");
+        a.set(3,"charan");
+
+        Iterator itr = a.iterator();
+        while (itr.hasNext()) {
+            System.out.println(itr.next());
+        }
+    }
+}
