1182d9d9c62e759bce7a49ee977ef025572dc544
LAPTOP-NHVKDFGE\jaswa <jaswanthg12@gmail.com>
2021-02-02 23:09:04 +0530

    Arrays

---
+++ src/february/Str1.java
@@ -0,0 +1,11 @@
+package february;
+
+public class Str1 {
+    public static void main(String[] args){
+        String txt = "Please where locate occurs!";
+        System.out.println(txt.indexOf("locate"));
+        System.out.println(txt.length());
+        System.out.println(txt.charAt(9));
+    }
+}
+
