73b498afc65eebb9815d16996f07d047a80ecc0b
LAPTOP-NHVKDFGE\jaswa <jaswanthg12@gmail.com>
2021-02-02 22:40:34 +0530

    Arrays

---
+++ src/february/DataType1.java
@@ -0,0 +1,16 @@
+package february;
+
+public class DataType1 {
+    public static void main(String[] args){
+        String s = "Jaswanth"; // default value = null;
+        Character c = 'G';
+        int n = 21;
+        float f = 9.87f;
+        double d = 7.98;
+        System.out.println(s);
+        System.out.println(c);
+        System.out.println(n);
+        System.out.println(f);
+        System.out.println(d);
+    }
+}
