ae0222e65e35ea058f79662a9049e62437e075dd
LAPTOP-NHVKDFGE\jaswa <jaswanthg12@gmail.com>
2021-02-03 21:19:29 +0530

    opertaors

---
+++ src/february/Operator1.java
@@ -0,0 +1,20 @@
+package february;
+
+public class Operator1 {
+    public static void main(String[] args){
+        int x = 12;
+        int y = 10;
+         int add,sub,mul, div, mod;
+         add = x+y;
+         sub = x-y;
+         mul = x*y;
+         div = x/y;
+         mod = x%y;
+         System.out.println(add);
+        System.out.println(sub);
+        System.out.println(mul);
+        System.out.println(div);
+        System.out.println(mod);
+        System.out.println(x+=5);
+    }
+}
