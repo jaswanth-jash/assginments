407ddefd68e351d86b12f0fdb855418a14fb72f9
LAPTOP-NHVKDFGE\jaswa <jaswanthg12@gmail.com>
2021-02-03 21:31:42 +0530

    opertaors

---
+++ src/february/Operator2.java
@@ -0,0 +1,14 @@
+package february;
+
+public class Operator2 {
+    public static void main(String[] args){
+        int a = 25;
+        int b = 15;
+        if(a==b){
+            System.out.println("true");
+        }
+        else{
+            System.out.println("false");
+        }
+    }
+}
