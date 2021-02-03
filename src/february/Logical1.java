075292a9bb8d523544036712c5db6dcc4d1ce97c
LAPTOP-NHVKDFGE\jaswa <jaswanthg12@gmail.com>
2021-02-03 21:50:01 +0530

    opertaors

---
+++ src/february/Logical1.java
@@ -0,0 +1,11 @@
+package february;
+
+public class Logical1 {
+    public static void main(String[] args){
+        int x = 10;
+            System.out.println(x<5 && x<11);
+        System.out.println(x<5 || x<11);
+        System.out.println(!(x<5 && x<11));
+        }
+    }
+
