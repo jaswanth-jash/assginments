8abac388731cd0c262bd93f3ad9979916dd0bf76
LAPTOP-NHVKDFGE\jaswa <jaswanthg12@gmail.com>
2021-02-05 22:45:52 +0530

    loops

---
+++ src/february/Ternary2.java
@@ -0,0 +1,9 @@
+package february;
+
+public class Ternary2 {
+    public static void main(String[] args){
+        int n = 15;
+        String n1 = (n % 2 ==0 ) ? " null" : "****";
+        System.out.println(n1);
+    }
+}
