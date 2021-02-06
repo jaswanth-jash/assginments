984abbdf1717d138175ac1b4f6b39e343035acc1
LAPTOP-NHVKDFGE\jaswa <jaswanthg12@gmail.com>
2021-02-06 22:30:06 +0530

    loops

---
+++ src/february/Nested1.java
@@ -0,0 +1,14 @@
+package february;
+
+public class Nested1 {
+    public static void main(String[] args) {
+        int age = 20;
+        int weight = 80;
+        //applying condition on age and weight
+        if (age >= 18) {
+            if (weight > 50) {
+                System.out.println("You are eligible to donate blood");
+            }
+        }
+    }
+}
