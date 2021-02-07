3c1a82a298a720efabfb7fa660f62cd2fe6f67bc
LAPTOP-NHVKDFGE\jaswa <jaswanthg12@gmail.com>
2021-02-07 22:06:58 +0530

    loops

--- src/february/Nested2.java
+++ src/february/Nested2.java
@@ -2,8 +2,8 @@ package february;
 
 public class Nested2 {
     public static void main(String[] args) {
-        int age = 25;
-        int weight = 48;
+        int age = 15;
+        int weight = 55;
         //applying condition on age and weight
         if (age >= 18) {
             if (weight > 50) {
