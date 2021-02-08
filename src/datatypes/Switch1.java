e804ec05fbb07c49ad1feab4a9d084a43b09c3c1
LAPTOP-NHVKDFGE\jaswa <jaswanthg12@gmail.com>
2021-02-08 21:12:02 +0530

    switch

---
+++ src/datatypes/Switch1.java
@@ -0,0 +1,19 @@
+package datatypes;
+
+public class Switch1 {
+    public static void main(String[] args) {
+        int number = 20;
+        //switch expression with int value
+        switch (number) {
+            //switch cases without break statements
+            case 1:
+                System.out.println("10");
+            case 2:
+                System.out.println("20");
+            case 3:
+                System.out.println("30");
+            default:
+                System.out.println("Not in 10, 20 or 30");
+        }
+    }
+}
