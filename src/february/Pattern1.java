6d48637968d2961fe6d67f98e69a1156a2a007da
LAPTOP-NHVKDFGE\jaswa <jaswanthg12@gmail.com>
2021-02-17 21:45:33 +0530

    for loops

--- src/february/Pattern1.java
+++ src/february/Pattern1.java
@@ -2,10 +2,11 @@ package february;
 
 public class Pattern1 {
     public static void main(String[] args) {
+        int n = 6;
 
-        for(int i=1;i<=5;i++){
+        for(int i=1;i<=n;i++){
 
-            for(int j=1;j<=i;j++){
+            for(int j=n;j>=i;j--){
                 System.out.print("@");
             }
             System.out.println();
