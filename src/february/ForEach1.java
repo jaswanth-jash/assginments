e02906279997d75b1419652d1481ff4be053ec10
LAPTOP-NHVKDFGE\jaswa <jaswanthg12@gmail.com>
2021-02-17 21:53:48 +0530

    for loops

---
+++ src/february/ForEach1.java
@@ -0,0 +1,14 @@
+package february;
+
+public class ForEach1 {
+    public static void main(String[] args){
+        String a[] ={"jashu", "jay", "ayyappa","yogith","sai" };
+       // int i [] ={11,12,13,14,15};
+        for(String s : a){
+
+                System.out.println(s);
+            }
+
+        }
+    }
+
