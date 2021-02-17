e090b67f60084854dc1b5e50272e1e6462e0c807
LAPTOP-NHVKDFGE\jaswa <jaswanthg12@gmail.com>
2021-02-17 21:55:51 +0530

    for loops

--- src/february/ForEach1.java
+++ src/february/ForEach1.java
@@ -2,13 +2,14 @@ package february;
 
 public class ForEach1 {
     public static void main(String[] args){
-        String a[] ={"jashu", "jay", "ayyappa","yogith","sai" };
-       // int i [] ={11,12,13,14,15};
-        for(String s : a){
+      //  String a[] ={"jashu", "jay", "ayyappa","yogith","sai" };
+        int i [] ={11,12,13,14,15};
+        //for(String s : a){
+            for (int b : i){
 
-                System.out.println(s);
+                System.out.println(b);
             }
 
-        }
+      //  }
     }
-
+}
