a76aa81b10e8798a00bd7ee7f2a80ee5d1f74e6d
LAPTOP-NHVKDFGE\jaswa <jaswanthg12@gmail.com>
2021-02-18 21:56:10 +0530

    for loops

---
+++ src/february/Break1.java
@@ -0,0 +1,21 @@
+package february;
+
+public class Break1 {
+    public static void main(String[] args){
+//        for(int i=1;i<=10;i++){
+//            if(i==5){
+//                //breaking the loop
+//                continue;//skip the value
+//            }
+//            System.out.println(i);
+//        }
+        int i =1;
+        while (i<=6){
+            if (i == 4) {
+                break;
+            }
+            System.out.println(i);
+            i++;
+        }
+    }
+}
