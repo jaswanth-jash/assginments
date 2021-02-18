b6e57bac7ab9250d9b86cf7680a671d905f8e365
LAPTOP-NHVKDFGE\jaswa <jaswanthg12@gmail.com>
2021-02-18 21:38:21 +0530

    for loops

---
+++ src/february/Do1.java
@@ -0,0 +1,11 @@
+package february;
+
+public class Do1 {
+    public static void main(String[] args){
+        int i =1;
+        do {
+            System.out.println(i);
+            i++;
+        }while (i<=5);
+    }
+}
