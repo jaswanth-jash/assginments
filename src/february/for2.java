ca1279cd4ebb1d9b39fac972c4bda8205e7886a6
LAPTOP-NHVKDFGE\jaswa <jaswanthg12@gmail.com>
2021-02-19 22:15:36 +0530

    for loops

--- src/february/for2.java
+++ src/february/for2.java
@@ -3,9 +3,9 @@ package february;
 public class for2 {
     public static void main(String[] args) {
 
-        for (int i = 1; i <= 3; i++) {
+        for (int i = 1; i <= 4; i++) {
 
-            for (int j = 1; j <= 3; j++) {
+            for (int j = 1; j <= 5; j++) {
                 System.out.println(i + " " + j);
             }//end of i
         }//end of j
