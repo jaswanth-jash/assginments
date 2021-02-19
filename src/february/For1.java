7abfc81ecad390a05cce5fabfbb599ffb573e0d3
LAPTOP-NHVKDFGE\jaswa <jaswanthg12@gmail.com>
2021-02-19 22:22:38 +0530

    for loops

--- src/february/For1.java
+++ src/february/For1.java
@@ -2,8 +2,16 @@ package february;
 
 public class For1 {
     public static void main(String[] args) {
-        for (int i = 1; i <= 5; i++) {
-            System.out.println(i);
+//        for (int i = 1; i <= 5; i++) {
+//            System.out.println(i);
+//        }
+
+        int i = 5;
+        if (i%2==0){
+            System.out.println("even number");
+        }
+        else {
+            System.out.println("odd number");
         }
     }
 }
