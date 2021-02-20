e0362e9e6fa7c398d3cab31bb5e9ae735634f0cd
LAPTOP-NHVKDFGE\jaswa <jaswanthg12@gmail.com>
2021-02-20 21:40:33 +0530

    for loops

--- src/february/Factorial1.java
+++ src/february/Factorial1.java
@@ -2,11 +2,15 @@ package february;
 
 public class Factorial1 {
     public static void main(String[] args) {
-        int num = 5, i = 1;
+        int num = 10;
         long factorial = 1;
-        while (i <= num) {
+//        while (i <= num) {
+//            factorial *= i;
+//            i++;
+//        }
+//        System.out.printf("Factorial of %d = %d", num, factorial);
+        for (int i =1;i<=num;i++){
             factorial *= i;
-            i++;
         }
         System.out.printf("Factorial of %d = %d", num, factorial);
     }
