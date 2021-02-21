9dfb8b8f32b4f4e8c55dda596d6123e8527e58b7
LAPTOP-NHVKDFGE\jaswa <jaswanthg12@gmail.com>
2021-02-21 22:24:43 +0530

    factorial

--- src/february/for2.java
+++ src/february/for2.java
@@ -5,7 +5,7 @@ public class for2 {
 
         for (int i = 1; i <= 4; i++) {
 
-            for (int j = 1; j <= 5; j++) {
+            for (int j = 1; j <= 8; j++) {
                 System.out.println(i + " " + j);
             }//end of i
         }//end of j
