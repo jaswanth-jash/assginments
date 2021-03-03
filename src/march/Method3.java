a53b426d98365004baa7c1621e576c8167792482
LAPTOP-NHVKDFGE\jaswa <jaswanthg12@gmail.com>
2021-03-03 20:57:46 +0530

    methods

--- src/march/Method3.java
+++ src/march/Method3.java
@@ -2,7 +2,7 @@ package march;
 
 public class Method3 {
     public int addNumbers(int a, int b) {
-        int mul = a - b;
+        int mul = a * b;
         // return value
         return mul;
     }
@@ -10,12 +10,12 @@ public class Method3 {
     public static void main(String[] args) {
 
         int num1 = 20;
-        int num2 = 10;
+        int num2 = 15;
 
         // create an object of Main
         Method3 obj = new Method3();
         // calling method
         int result = obj.addNumbers(num1, num2);
-        System.out.println("Sub is: " + result);
+        System.out.println("mul is: " + result);
     }
 }
