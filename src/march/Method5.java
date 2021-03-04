4755fc82f38f1314ab14df68f49f3e7e85cbf6e1
LAPTOP-NHVKDFGE\jaswa <jaswanthg12@gmail.com>
2021-03-04 21:54:43 +0530

    methods

--- src/march/Method5.java
+++ src/march/Method5.java
@@ -2,10 +2,10 @@ package march;
 
 public class Method5 {
     void method5(){
-
+    System.out.println("no argument");
     }
     void method5(int a , int b){
-
+    System.out.println("parameterized");
     }
     public static void main(String [] args){
 
