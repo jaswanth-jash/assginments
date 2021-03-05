3aabd41e261aa1ad3736816c9c975a7618a51059
LAPTOP-NHVKDFGE\jaswa <jaswanthg12@gmail.com>
2021-03-05 21:17:08 +0530

    for each

---
+++ src/march/ForEach1.java
@@ -0,0 +1,24 @@
+package march;
+
+import java.util.Scanner;
+
+public class ForEach1 {
+
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("enter the name");
+                String a = scan.nextLine();
+        System.out.println("name is " + a);
+        scan.close();
+
+
+
+
+      //  String a[] = {"jashu", "sai", "deepthi"};
+//        for (String b : a) {
+//            System.out.println(b);
+//
+//        }
+    }
+}
