1c5abff9d825d7a6028de71f1383eba0abfee265
LAPTOP-NHVKDFGE\jaswa <jaswanthg12@gmail.com>
2021-03-09 21:35:17 +0530

    for each

---
+++ src/march/Method6.java
@@ -0,0 +1,22 @@
+package march;
+
+public class Method6 {
+    private String formatNumber(int value) {
+        return String.format("%d", value);
+    }
+
+    private String formatNumber(double value) {
+        return String.format("%.3f", value);
+    }
+
+    private String formatNumber(String value) {
+        return String.format("%.2f", Double.parseDouble(value));
+    }
+
+    public static void main(String[] args) {
+        Method6 hs = new Method6();
+        System.out.println(hs.formatNumber(500));
+        System.out.println(hs.formatNumber(89.9934));
+        System.out.println(hs.formatNumber("550"));
+    }
+}
