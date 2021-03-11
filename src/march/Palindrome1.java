a7475506e58d31f51fdf36d622ec1fab9798ebc8
LAPTOP-NHVKDFGE\jaswa <jaswanthg12@gmail.com>
2021-03-11 13:38:33 +0530

    arrays

---
+++ src/march/Palindrome1.java
@@ -0,0 +1,19 @@
+package march;
+
+public class Palindrome1 {
+    public static void main(String[] args) {
+        String s = "MADAM" ;
+        s = s.trim();
+        String res = " ";
+        for (int i = s.length() - 1; i >= 0; i--) {
+            res = res + s.charAt(i);
+        }
+        if (res.equals(s))
+
+            System.out.print("palindrome");
+
+        else
+            System.out.print("Not palindrome");
+
+    }
+}
