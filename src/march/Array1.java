292e9b07f1881abd5255c30c2fa4ccf3eceae534
LAPTOP-NHVKDFGE\jaswa <jaswanthg12@gmail.com>
2021-03-11 08:31:00 +0530

    arrays

---
+++ src/march/Array1.java
@@ -0,0 +1,18 @@
+package march;
+
+public class Array1 {
+    public static void main(String[] args) {
+
+        //Initialize array
+        int[] a = new int[]{1, 2, 3, 4, 2, 7, 8, 8, 3};
+
+        System.out.println("Duplicate elements in given array: ");
+        //Searches for duplicate element
+        for (int i = 0; i < a.length; i++) {
+            for (int j = i + 1; j < a.length; j++) {
+                if (a[i] == a[j])
+                    System.out.println(a[j]);
+            }
+        }
+    }
+}
