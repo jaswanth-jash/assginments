93e485029623e4382a90632de1b9ee681939a4d6
LAPTOP-NHVKDFGE\jaswa <jaswanthg12@gmail.com>
2021-03-29 22:07:53 +0530

    prgms

--- src/march/Palindrome2.java
+++ src/march/Palindrome2.java
@@ -3,7 +3,7 @@ package march;
 public class Palindrome2 {
     public static void main(String args[]){
         int r,sum=0,temp;
-        int n=797;//It is the number variable to be checked for palindrome
+        int n=798;//It is the number variable to be checked for palindrome
 
         temp=n;
         while(n>0){
